851af42af9efc8af703f63125ae1c7a14c6d5a2d
ohidulalam <mdnoyonctg15@gmail.com>
2022-06-08 12:49:56 +0600

    slide 1-2 solve

---
+++ ICS-basic/Class Problem/CylenderVolume.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+float main()
+{
+
+     float height, radius, pi, volume;
+     pi = acos(-1);
+     printf("Enter the Height & Radius of the Cylinder : \n");
+     scanf("%f%f", &height, &radius);
+     volume = pi * radius * radius * height;
+
+     printf("The Volume of The Cylinder is : %.2f", volume);
+
+     return 0;
+}
